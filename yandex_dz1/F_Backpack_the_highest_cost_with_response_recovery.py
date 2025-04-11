68ddf7c8d75fedcfd8241a4747a6df4a40fcc31b
NikitaNedelko <nedelkonikita345@gmail.com>
2025-04-11 21:56:30 +0300

    Добавил задачу 1 F. Рюкзак: наибольшая стоимость с восстановлением ответа

--- yandex_dz1/F_Backpack_the_highest_cost_with_response_recovery.py
+++ yandex_dz1/F_Backpack_the_highest_cost_with_response_recovery.py
@@ -1,4 +1,4 @@
-"""E. Рюкзак: наибольшая стоимость
+"""F. Рюкзак: наибольшая стоимость с восстановлением ответа
 
 Скорость работы: O (N * M)
 Затрачено памяти: O (M)
@@ -15,16 +15,26 @@ def main():
     for i in range(len(weight)):  # O(N) по времени
         golds[i].append(weight[i])
         golds[i].append(price[i])
+        golds[i].append(i + 1)
 
     backpack = [0 for _ in range(M + 1)]  # O(M+1) по времени
+    choice: list[list[int]] = [[] for _ in range(M + 1)]  # O(M+1) по времени
 
-    for weight, price in golds:  # O(N) по времени
+    for weight, price, id in golds:  # O(N) по времени
         for j in range(M, weight - 1, -1):  # O(M - i) по времени
             if backpack[j - weight] + price > backpack[j]:
                 backpack[j] = backpack[j - weight] + price
+                choice[j] = choice[j - weight] + [id]
 
-    print(backpack[M])
+    for i in choice[M]:  # O(M) по времени
+        print(i)
 
 
 if __name__ == "__main__":
     main()
+
+"""
+4 6
+2 4 1 2
+7 2 5 1
+"""
