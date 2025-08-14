c8bd3c95b611fcb2c23d0bf06d0d56b722946e98
NikitaNedelko <nedelkonikita345@gmail.com>
2025-08-14 12:44:08 +0300

    немного ускорил работу

--- COLLECTIONS/two pointers/849_Maximize_Distance_to_Closest_Person.py
+++ COLLECTIONS/two pointers/849_Maximize_Distance_to_Closest_Person.py
@@ -18,16 +18,20 @@ class Solution(object):
         left_index = -1
         right_index = -1
 
+        len_nums = len(seats)
+
         for index, seat in enumerate(seats):
             if seat == 1:
                 right_index = index
                 if left_index == -1:
-                    max_len = max(max_len, right_index)
+                    max_len = max_len if max_len > right_index else right_index
                 else:
-                    max_len = max(max_len, (right_index - left_index) // 2)
+                    current_len = (right_index - left_index) // 2
+                    max_len = max_len if max_len > current_len else current_len
                 left_index = right_index
 
-        if right_index != len(seats) - 1:
-            max_len = max(max_len, len(seats) - 1 - right_index)
+        if right_index != len_nums - 1:
+            current_len = len_nums - 1 - right_index
+            max_len = max_len if max_len > current_len else current_len
 
         return max_len
